dd6154e8cbcd879031724116e8937f7fc350bb81
Wagner Corrêa <wagnerspg@hotmail.com>
2023-01-12 21:41:33 -0300

    Create Customer.php
    Added Class Customer in the project.

---
+++ src/Getnet/Api/Customer.php
@@ -0,0 +1,211 @@
+<?php
+	
+	namespace Source\Api;
+	
+	use Source\Api\Address;
+	use Source\Api\Traits\TraitEntity;
+	
+	/**
+	 * Class Customer
+	 *
+	 * @package Source\Api
+	 *
+	 * @author Wagner Corrêa
+	 */
+	class Customer implements \JsonSerializable
+	{
+		use TraitEntity;
+		
+		const DOCUMENT_TYPE_CPF = "CPF";
+		const DOCUMENT_TYPE_CNPJ = "CNPJ";
+		
+		private $customer_id;
+		private $first_name;
+		private $last_name;
+		private $name;
+		private $email;
+		private $document_type;
+		private $document_number;
+		private $phone_number;
+		private $billing_address;
+		
+		public function __construct($customer_id = null)
+		{
+			$this->setCustomerId($customer_id);
+		}
+		
+		/**
+		 *
+		 * @return mixed
+		 */
+		public function getCustomerId()
+		{
+			return $this->customer_id;
+		}
+		
+		/**
+		 *
+		 * @param mixed $customer_id
+		 */
+		public function setCustomerId($customer_id): string
+		{
+			return $this->customer_id = (string) $customer_id;
+		}
+		
+		/**
+		 *
+		 * @return mixed
+		 */
+		public function getFirstName()
+		{
+			return $this->first_name;
+		}
+		
+		/**
+		 *
+		 * @param mixed $first_name
+		 */
+		public function setFirstName($first_name): string
+		{
+			return $this->first_name = (string) $first_name;
+		}
+		
+		/**
+		 *
+		 * @return mixed
+		 */
+		public function getLastName()
+		{
+			return $this->last_name;
+		}
+		
+		/**
+		 *
+		 * @param mixed $last_name
+		 */
+		public function setLastName($last_name): string
+		{
+			return $this->last_name = (string) $last_name;
+		}
+		
+		/**
+		 *
+		 * @return mixed
+		 */
+		public function getName()
+		{
+			return $this->name;
+		}
+		
+		/**
+		 *
+		 * @param mixed $name
+		 */
+		public function setName($name): string
+		{
+			return $this->name = (string) $name;
+		}
+		
+		/**
+		 *
+		 * @return mixed
+		 */
+		public function getEmail()
+		{
+			return $this->email;
+		}
+		
+		/**
+		 *
+		 * @param mixed $email
+		 */
+		public function setEmail($email): string
+		{
+			return $this->email = (string) $email;
+		}
+		
+		/**
+		 *
+		 * @return mixed
+		 */
+		public function getDocumentType()
+		{
+			return $this->document_type;
+		}
+		
+		/**
+		 *
+		 * @param mixed $document_type
+		 */
+		public function setDocumentType($document_type): string
+		{
+			return $this->document_type = (string) $document_type;
+		}
+		
+		/**
+		 *
+		 * @return mixed
+		 */
+		public function getDocumentNumber()
+		{
+			return $this->document_number;
+		}
+		
+		/**
+		 *
+		 * @param mixed $document_number
+		 */
+		public function setDocumentNumber($document_number): string
+		{
+			return $this->document_number = (string) $document_number;
+		}
+		
+		/**
+		 *
+		 * @return mixed
+		 */
+		public function getPhoneNumber()
+		{
+			return $this->phone_number;
+		}
+		
+		/**
+		 *
+		 * @param mixed $phone_number
+		 */
+		public function setPhoneNumber($phone_number): string
+		{
+			return $this->phone_number = (string) $phone_number;
+		}
+		
+		/**
+		 *
+		 * @return Address
+		 */
+		public function billingAddress(): Address
+		{
+			$address = new Address();
+			
+			$this->setBillingAddress($address);
+			
+			return $address;
+		}
+		
+		/**
+		 *
+		 * @return Address
+		 */
+		public function getBillingAddress(): Address
+		{
+			return $this->billing_address;
+		}
+		
+		/**
+		 *
+		 * @param mixed $billing_address
+		 */
+		public function setBillingAddress($billing_address)
+		{
+			return $this->billing_address = $billing_address;
+		}
+	}
